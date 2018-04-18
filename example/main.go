575b6046130d3cce3f60c230e320e84a130e43a0
Jesse <Jesseokeya@gmail.com>
2018-04-17 22:24:43 -0400

    changes ✓

--- example/main.go
+++ example/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+type Users struct {
+	id        int
+	firstName string
+	lastName  string
+	email     string
+	password  string
+	address   string
+}
 
+func main() {
+	databaseName := "myAppUsers"
+	schema := Users{
+		id:        21334130,
+		firstName: "Jesse",
+		lastName:  "Okeya",
+		password:  "encrypted",
+		email:     "Jesseokeya@gmail.com",
+		address:   "2550 Cotters Crescent, K1V8Y6",
+	}
+
+	//filestorage.initlialize(schema, databaseName)
 }
