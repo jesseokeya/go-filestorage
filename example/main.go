084ff7805a8cacf0afbaae0c61082b8b6d398d6c
Jesse <Jesseokeya@gmail.com>
2018-04-18 09:46:53 -0400

    bug fix ✓

--- example/main.go
+++ example/main.go
@@ -1,25 +1,25 @@
 package main
 
-type Users struct {
-	id        int
-	firstName string
-	lastName  string
-	email     string
-	password  string
-	address   string
+// schema struct is a schema for app users
+type schema struct {
+	ID        int    `json:"_id,omitempty"`
+	FirstName string `json:"firtsName,omitempty"`
+	LastName  string `json:"lastName,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Password  string `json:"password,omitempty"`
+	Address   string `json:"address,omitempty"`
 }
 
 func main() {
+	user := schema{
+		ID:        21334131,
+		FirstName: "Jesse",
+		LastName:  "Okeya",
+		Password:  "encrypted",
+		Email:     "Jesseokeya@gmail.com",
+		Address:   "2550 Cotters Crescent, K1V8Y6",
+	}
 
-	// schema := Users{
-	// 	id:        21334130,
-	// 	firstName: "Jesse",
-	// 	lastName:  "Okeya",
-	// 	password:  "encrypted",
-	// 	email:     "Jesseokeya@gmail.com",
-	// 	address:   "2550 Cotters Crescent, K1V8Y6",
-	// }
-
-	//database := filestorage.Initialize("App Users")
-
+	database := filestorage.Connect()
+	database.Name("Jesse's Users")
 }
